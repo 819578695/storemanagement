8dd9d1e52e0de89edaed411ae23f1210ef02df73
shenweikang <shenweikang@zgt56.com>
2019-08-19 17:30:05 +0800

    update

--- eladmin-logging/src/main/java/me/zhengjie/service/mapper/LogErrorMapper.java
+++ eladmin-logging/src/main/java/me/zhengjie/service/mapper/LogErrorMapper.java
@@ -13,4 +13,5 @@ import org.mapstruct.ReportingPolicy;
 @Mapper(componentModel = "spring",uses = {},unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface LogErrorMapper extends EntityMapper<LogErrorDTO, Log> {
 
+
 }
