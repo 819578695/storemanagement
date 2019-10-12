f0055631b621e87a0b224aa3047a8bd427c5dfcd
Mingkun_Niu <mingkun_niu@126.com>
2019-10-12 11:13:10 +0800

    1

--- eladmin-system/src/main/java/me/zhengjie/modules/business/domain/ReceiptPaymentAccount.java
+++ eladmin-system/src/main/java/me/zhengjie/modules/business/domain/ReceiptPaymentAccount.java
@@ -5,6 +5,7 @@ import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.bean.copier.CopyOptions;
 import javax.persistence.*;
 import java.io.Serializable;
+import java.math.BigDecimal;
 
 /**
 * @author kang
@@ -24,33 +25,25 @@ public class ReceiptPaymentAccount implements Serializable {
     @Column(name = "name")
     private String name;
 
-    // 部门id关联
-    @Column(name = "dept_id")
-    private Long deptId;
+    //金额
+    @Column(name = "remaining")
+    private BigDecimal remaining;
 
-    // 付款账户名称
-    @Column(name = "payment_account")
-    private String paymentAccount;
+    // 账户详情id
+    @Column(name = "detail_id")
+    private Long detailId;
+
+    // 账户名称
+    @Column(name = "account_name")
+    private String accountName;
 
     // 付款账号
-    @Column(name = "payment_account_num")
-    private String paymentAccountNum;
+    @Column(name = "account_num")
+    private String accountNum;
 
     // 付款开户行
-    @Column(name = "payment_bank")
-    private String paymentBank;
-
-    // 收款账户名称
-    @Column(name = "receipt_account")
-    private String receiptAccount;
-
-    // 收款账号
-    @Column(name = "receipt_account_num")
-    private String receiptAccountNum;
-
-    // 收款开户行
-    @Column(name = "receipt_bank")
-    private String receiptBank;
+    @Column(name = "bank_name")
+    private String bankName;
 
     public void copy(ReceiptPaymentAccount source){
         BeanUtil.copyProperties(source,this, CopyOptions.create().setIgnoreNullValue(true));
--- eladmin-system/src/main/java/me/zhengjie/modules/business/repository/ReceiptPaymentAccountRepository.java
+++ eladmin-system/src/main/java/me/zhengjie/modules/business/repository/ReceiptPaymentAccountRepository.java
@@ -11,9 +11,9 @@ import java.util.Set;
 * @date 2019-08-21
 */
 public interface ReceiptPaymentAccountRepository extends JpaRepository<ReceiptPaymentAccount, Long>, JpaSpecificationExecutor {
-    @Query(value = "select * from receipt_payment_account where id = ?1",nativeQuery = true)
-    ReceiptPaymentAccount findAllById(Long id);
+    //@Query(value = "select * from receipt_payment_account where id = ?1",nativeQuery = true)
+    //ReceiptPaymentAccount findAllById(Long id);
 
-    List<ReceiptPaymentAccount> findByDeptId(Long deptId);
+    //List<ReceiptPaymentAccount> findByDeptId(Long deptId);
 
 }
